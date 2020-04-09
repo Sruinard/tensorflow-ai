d1d500a717caa00d3426ed71dfc7434ac865719a
Stef Ruinard <stef.ruinard@ml6.eu>
2020-04-09 08:04:44 +0200

    added support for tflite

--- main.py
+++ main.py
@@ -30,36 +30,57 @@ def main(model_version):
 
 
 def fine_tune_full(model_version):
-    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
-    x_train = preprocessing_func(x_train)
-    x_test = preprocessing_func(x_test)
-    path = f'./my_full_model/{model_version}/'
-    model = tf.keras.models.load_model(path)
-    model.fit(x_train, y_train, epochs=20, validation_data=(x_test, y_test))
-
-    model.evaluate(x_test, y_test)
-
-    path = f'./my_model/{model_version+1}/'
-    model.save(path)
-    print(y_test[:10])
-    np.save('test_data', x_test[:10])
-
-def fine_tune(model_version):
-    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
-    x_train = preprocessing_func(x_train)
-    x_test = preprocessing_func(x_test)
     path = f'./my_model/{model_version}/'
-    model = tf.keras.models.load_model(path)
-    model.fit(x_train, y_train, epochs=20, validation_data=(x_test, y_test))
 
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+
+    PreProcessingLayer = Preprocess()
+    PostProcessingLayer = Postprocess()
+
+    x_train = PreProcessingLayer(x_train)
+    x_test = PreProcessingLayer(x_test)
+
+    model = tf.keras.models.load_model(path)
+
+    model.fit(x_train, y_train, epochs=1, validation_data=(x_test, y_test))
     model.evaluate(x_test, y_test)
 
-    path = f'./my_model/{model_version+1}/'
+    path = f'./my_model/{model_version + 1}/'
     model.save(path)
-    print(y_test[:10])
-    np.save('test_data', x_test[:10])
+
+    path_full = f'./my_full_model/{model_version + 1}/'
+    inference_model = ConvertModel(PreProcessingLayer, model, PostProcessingLayer)
+    inference_model.predict(x_test[:10])
+    inference_model.save(path_full)
+
+def convert_to_tflite(model_version):
+    path = f'./my_model/{model_version}/'
+
+    (_, _), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+
+    PreProcessingLayer = Preprocess()
+    PostProcessingLayer = Postprocess()
+
+    x_test = PreProcessingLayer(x_test)
+
+    model = tf.keras.models.load_model(path)
+
+    inference_model = ConvertModel(PreProcessingLayer, model, PostProcessingLayer)
+    inference_model.predict(x_test[:10])
+
+    converter = tf.lite.TFLiteConverter.from_keras_model(inference_model)
+    tflite_model = converter.convert()
+    open("./my_tflite_models/converted_model.tflite", "wb").write(tflite_model)
+
+
+
+
 
 
 if __name__ == '__main__':
-    #fine_tune_full(1)
-    main(1)
+    test_tflite()
+
+
+    # main(1)
+    # fine_tune_full(1)
+    # convert_to_tflite(2)
