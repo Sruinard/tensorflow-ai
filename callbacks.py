640aa8eb58624566f4bd17896c955207b20a423c
Stef Ruinard <stef.ruinard@ml6.eu>
2020-04-10 10:27:29 +0200

    pylint callbacks.py

--- callbacks.py
+++ callbacks.py
@@ -1,20 +1,56 @@
+""" File for writing custom keras callbacks """
+# pylint: disable=W0201
+
 import tensorflow as tf
 import numpy as np
 
 from model import ConvertModel
 
-class SaveProductionModel(tf.keras.callbacks.Callback):
+class SaveProductionModel(tf.keras.callbacks.Callback): # pylint: disable=R0902
+    """save the best model for use in production"""
 
-    def __init__(self, patience=2):
+    def __init__(
+            self,
+            pre_processing,
+            post_processing,
+            data_batch,
+            path_to_store_model,
+            patience=2
+    ):
+        """
+
+        Args:
+            pre_processing: function to include in model
+            post_processing: function to include in model
+            data_batch: train batch to init shapes
+            path_to_store_model: where to save production model
+            patience: time to wait before stop training and save production model
+        """
         super(SaveProductionModel).__init__()
+        self.pre_processing = pre_processing
+        self.post_processing = post_processing
+        self.data_batch = data_batch
+        self.path_to_store_model = path_to_store_model
         self.patience = patience
 
     def on_train_begin(self, logs=None):
-        self.wait = 0
+        """
+        action to take when start training
+        Args:
+            logs: model logs for accessing attributes
+        """
 
+        self.wait = 0
         self.best = np.Inf
 
     def on_epoch_end(self, epoch, logs=None):
+        """
+        check what best model weights are and determine to stop training
+        Args:
+            epoch: current epoch number
+            logs: model logs for accessing attributes
+        """
+
         current_loss = logs['loss']
 
         if current_loss < self.best:
@@ -30,4 +66,8 @@ class SaveProductionModel(tf.keras.callbacks.Callback):
 
 
     def on_train_end(self, logs=None):
-        pass
+        """ TODO load best model weights and save """
+        # TODO get best weights
+        model = ConvertModel(self.pre_processing, self.model, self.post_processing)
+        model.predict(self.data_batch)
+        model.save(self.path_to_store_model)
